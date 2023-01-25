5a9474b165fc5eb6559a95b4dd365546b0d9e96b
Dellapino <ali.ichs2012@gmail.com>
2023-01-25 16:21:44 +0000

    SI model done

--- SI_model.py
+++ SI_model.py
@@ -11,6 +11,9 @@ import numpy as np
 import random
 
 def intitialise_potential(N, initial, threshold): # initial defines the fraction of populations that is initially infected
+    '''
+    Randomly infects a given fraction of the popualtion
+    '''
     P = {} # another graph to keep track of potential
     nodes = N.keys()
     for node in nodes:
@@ -20,23 +23,7 @@ def intitialise_potential(N, initial, threshold): # initial defines the fraction
             P[node] = 0
     return P
 
-def propagate_SI(P, N, threshold, strength = 1, beta = 0.6): # add checks for valid parameter values
-    connections = nm.find_connections(N)
-    strengths = nm.find_strengths(N)
-    potentials = list(P.values())
-    size = len(N)
-    F = np.zeros(size)
-    for i in range(size): # loop over every node and check if firing
-        if potentials[i] >= threshold:
-            if np.random.random() < beta:
-                F[i] = 1
-        for j in range(len(connections[i])):    
-            if np.random.random() < beta: #0.6 is placeholder value for beta
-                P[connections[i][j]] += strengths[i][j] * strength   
-    return P, F
-
-
-def propagate_SI_v2(P, N, threshold, strength = 1, beta = 0.6): # add checks for valid parameter values
+def propagate(P, N, threshold, strength = 1, beta = 0.6): # add checks for valid parameter values
     '''
     propagates contagion according to transmission prob and infection strength
     '''
@@ -62,30 +49,19 @@ def activity(P, threshold):
         if potentials[i] >= threshold:
             activity += 1
     return activity / size
-    
 
-def simulate_SI(N, P, T, h, threshold):
-    size = len(N)
-    activity = []
-    limit = T 
-    for t in range(limit):
-        #time.sleep(1)
-        P, F = propagate_SI(P, N, threshold)
-        activity.append(sum(F) / size)
-    return activity
-
-def simulate_SI_v2(N, T, initial, threshold):
+def simulate(N, T, initial, threshold):
     '''
     Returns time series of infection activity up till time T
     '''
     activities = []
     P = intitialise_potential(N, initial, threshold)
     for t in range(T):
-        P = propagate_SI_v2(P, N, threshold)
+        P = propagate(P, N, threshold)
         activities.append(activity(P, threshold))
     return activities
 
-def smooth_SI_v2(N, T, initial, threshold, M): # M is number of runs over which to smooth over
+def smooth(N, T, initial, threshold, M): # M is number of runs over which to smooth over
     '''
     Smoothes infection activity time series by averaging over M runs
     '''
@@ -93,7 +69,7 @@ def smooth_SI_v2(N, T, initial, threshold, M): # M is number of runs over which 
     runs = []
     for i in range(M):
         P = intitialise_potential(N, initial, threshold)
-        run = simulate_SI_v2(N, T, initial, threshold)
+        run = simulate(N, T, initial, threshold)
         runs.append(run)
     for i in range(T):
         total = 0
@@ -103,24 +79,7 @@ def smooth_SI_v2(N, T, initial, threshold, M): # M is number of runs over which 
         smoothed.append(total)
     return smoothed
 
-def smooth_SI(N, I, T, M, h, threshold = 10): # M is number of runs over which to smooth over
-    runs = []
-    limit = int(T//h) + 1
-    limit = T
-    for i in range(M):
-        P = intitialise_potential(N, I, threshold)
-        run = simulate_SI(N, P, T, h, threshold)
-        runs.append(run)
-    smoothed = []
-    for i in range(limit):
-        total = 0
-        for j in range(M):
-            total += runs[j][i]     
-        total = total / M   
-        smoothed.append(total)
-    return smoothed
-
-def analytic_SI(x, beta, c):
+def analytic_sol(x, beta, c):
     return np.exp(beta*x+c) / (1 + np.exp(beta*x+c))
 
 def integration_const(initial):
@@ -131,19 +90,20 @@ def generate_t(T, h): # generates time axis from 0 to T with increments of h
 
 
 
-# EXPERIMENTAL
+# _________________EXPERIMENTAL_________________
+
+'''
+Optimize parameters to match model and analytic solution
+'''
 
 from scipy.optimize import curve_fit
 
-def SI(time, beta, h):
-    I_old = 0.05
-    for i in range(time):
-        I_new = h * beta * I_old * (1 - I_old) + I_old
-        I_old = I_new
-    return I_old
 
 
 # TO DO
 
 # function which takes input of all necessary parameters and returns either or all of model or analytical solution
 # should use the visualisation library to display these iA
+
+# Look at initialising in a non-discrete fashion -> have some at bit below threshold etc,
+# then can compare how initial condition affect final result more cleanly
